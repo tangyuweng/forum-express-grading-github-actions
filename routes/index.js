db6b2e1be8f8938b1f87feceeed4dd77f8ddd6f0
tangyuweng <tangyuw99@gmail.com>
2024-01-12 16:39:46 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin) // 導到後台 admin 路徑
+
+router.get('/signup', userController.singUpPage) // 取得登入頁
+
+router.post('/signup', userController.singUp) // 登入
 
 router.get('/restaurants', restController.getRestaurants)
 
